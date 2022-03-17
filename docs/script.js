5afa7da0b0c12c09f8f8f3f537c1714f7013d87b
Tom Schneider-Swales <thomas.schneider-swales@optile.net>
2022-03-17 12:56:41 +0100

    Undo setting cache header

--- docs/script.js
+++ docs/script.js
@@ -11,15 +11,7 @@ window.onload = function () {
 
 function fetchVersion(environment, versions) {
 
-    const myHeaders = new Headers();
-    myHeaders.append('cache-control', 'no-store');
-
-    var myInit = {
-        method: 'GET',
-        headers: myHeaders,
-    };
-
-    fetch("https://resources." + environment + ".oscato.com/paymentpage/v3/version", myInit)
+    fetch("https://resources." + environment + ".oscato.com/paymentpage/v3/version")
     .then(res => res.text())
     .then(version => {
         const trimmedVersion = version.trim()
